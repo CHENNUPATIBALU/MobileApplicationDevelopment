e7b0f525ab8e3112e8212a045f47d93acfceb6cf
Chennupati Balu <57926813+CHENNUPATIBALU@users.noreply.github.com>
2020-12-21 14:14:10 +0530

    Project updated

--- app/src/main/java/com/chennupatibalu/mobileapplicationdevelopmentcourse/IntentExample/WebsiteActivity.java
+++ app/src/main/java/com/chennupatibalu/mobileapplicationdevelopmentcourse/IntentExample/WebsiteActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -35,26 +36,41 @@ public class WebsiteActivity extends AppCompatActivity {
     }
 
     public void goToWebsite(View view) {
+        String defaultDomain = ".com";
         String urlText = urlEditText.getText().toString();
+        String domain = checkDomain(urlText);   //Checks the domain in the URL assigns to the variable
+
         String webUrl = "https://"+urlText;
         String url = "https://www.";
-        String domain = checkDomain(urlText);
-        String defaultDomain = ".com";
+        String nameUrl = url+urlText+defaultDomain;
+        Log.d("website", "goToWebsite: "+nameUrl);
 
+        //Checks the URL whether it is in the form "www.website.domain"
+        //Example: "www.google.com"
         if(urlText.startsWith("www.") && !urlText.startsWith("https://") && urlText.endsWith(domain))
         {
-            //Opens the URL which is in the form "www.website.domain"
             startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(webUrl)));
         }
+
+        //Checks the URL whether it is in the form "https://www.website.domain"
+        //Example: "https://www.google.com"
         if(urlText.startsWith("https://") && urlText.contains("www.") && urlText.endsWith(domain))
         {
-            //Opens the URL which is in the form "https://www.website.domain"
             startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(urlText)));
         }
-        if(!urlText.startsWith("www.") && !urlText.startsWith("https://") && urlText.endsWith(domain))
+
+        //Checks the URL whether it is in the form "website.domain"
+        //Example: "google.com"
+        if(!urlText.startsWith("https://") && !urlText.contains("www.") && urlText.endsWith(domain))
         {
-            //Opens the URL which is in the form "website.domain"
             startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url+urlText)));
+        }
+
+        //Checks the URL whether it is in the form "website"
+        //Example: "google"
+        if(!urlText.startsWith("https://") && !urlText.contains("www.") && !urlText.endsWith(domain))
+        {
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(nameUrl)));
         }
     }
     private String checkDomain(String url)
